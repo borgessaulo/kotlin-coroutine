82ab8444f84f6eb8e58e3cc6a6f31240bb7ab603
borgessaulo <borges.saulo@gmail.com>
2023-05-05 15:56:48 -0300

    app init

---
+++ src/main/kotlin/com/kotlin/coroutine/kotlincoroutine/KotlinCoroutineApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlin.coroutine.kotlincoroutine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinCoroutineApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinCoroutineApplication>(*args)
+}
